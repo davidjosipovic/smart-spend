7805fbf97c24fadaf232295b2d23e61f7935dec7
Leo Vukoje <leo.vukoje02@gmail.com>
2025-01-05 12:22:30 +0100

    Add user registration.

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
-from database import get_db
 from contracts.users.create_or_update_user import CreateOrUpdateUser
-import json
+from database import get_db
+from fastapi import APIRouter, Depends, HTTPException
+from model.users.user import User
+from model.common.response import Response
+from sqlalchemy.orm import Session
 
 router = APIRouter(
     tags=["Users"],
@@ -11,7 +12,23 @@ router = APIRouter(
 
 @router.post("/register")
 async def register(request: CreateOrUpdateUser, db: Session = Depends(get_db)):
-    return request
+    response = Response()
+
+    db_user = db.query(User).filter(User.email == request.emailAddress or User.username == request.username).first()
+    if db_user:
+        return response.with_error("Email or username is already registered.")
+
+    new_user = User(
+        name=request.name,
+        email=request.emailAddress,
+        username=request.username,
+        password=request.password
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
 
 #@router.post("/login")
 #async def login():
